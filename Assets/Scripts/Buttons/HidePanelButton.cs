936c37df68d39ec75aa80301028ec67cf0f587b8
Nicolae <riven064@gmail.com>
2021-03-28 11:54:25 +0100

    UI Build Work in progress

---
+++ Assets/Scripts/Buttons/HidePanelButton.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HidePanelButton : MonoBehaviour
+{
+    public string PanelId;
+
+    //Cached panel manager
+    private PanelManager _panelManager;
+
+    private void Start()
+    {
+        //Cache
+        _panelManager = PanelManager.Instance;
+    }
+    public void DoHidePanel ()
+    {
+        _panelManager.HideLastPanel(PanelId);
+    }
+}
